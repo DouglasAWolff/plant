70f720b2a2e6dd06c9fae95992f26c7b7c290786
Douglas Wolff <75222401+DouglasAWolff@users.noreply.github.com>
2023-05-07 11:29:38 +0100

    Update arduino.cpp

--- arduino.cpp
+++ arduino.cpp
@@ -1,81 +1,50 @@
 //Libraries
 #include <CapacitiveSensor.h>//https://github.com/PaulStoffregen/CapacitiveSensor
 //Parameters
-bool autocal   = 0;
-const int numReadings   = 1;
-long readings [numReadings];
-int readIndex   = 0;
-long total  = 0;
-const int sensitivity   = 1000;
-const int thresh  = 200;
-const int csStep  = 10000;
+const int sensitivity   = 10;
+
 int pcap1 = 0;
 int pcap2 = 0;
 int pcap3 = 0;
+
 CapacitiveSensor cs1   = CapacitiveSensor(2, 3);
 CapacitiveSensor cs2   = CapacitiveSensor(8, 9);
 CapacitiveSensor cs3   = CapacitiveSensor(12, 11);
+
 void setup() {
   pinMode(A0, OUTPUT);
   pinMode(A1, OUTPUT);
   pinMode(A2, OUTPUT);
   //Init Serial USB
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println(F("Initialize System"));
-  //Init cs
-  if (autocal == 0) {
-      {
-          cs1.set_CS_AutocaL_Millis(0xFFFFFFFF);
-      }
-  }
 }
+
 void loop() {
-  int cap1 = smooth1();
-  int cap2 = smooth2();
-  int cap3 = smooth3();
-  Serial.print("A: ");
-  Serial.println(cap2);
-  if (cap1 > 2000) {
-    if (pcap1 - cap1 > 500) {
-      digitalWrite(A0, LOW);
-    } else {
-      Serial.println("AHHHHHHHH");
-      digitalWrite(A0, HIGH);
-    }
+  int cap1 = cs1.capacitiveSensor(sensitivity);
+  int cap2 = cs2.capacitiveSensor(sensitivity);
+  int cap3 = cs3.capacitiveSensor(sensitivity);
+  if (pcap1 - cap1 > 500) {
+    digitalWrite(A0, HIGH);
+    Serial.println("High1");
   } else {
     digitalWrite(A0, LOW);  
   }
-  if (cap2 > 1500) {
-    if (pcap2 - cap2 > 500) {
-      digitalWrite(A1, LOW);
-    } else {
-      Serial.println("AHHHHHHHH");
-      digitalWrite(A1, HIGH);
-    }
+  if (pcap2 - cap2 > 500) {
+    digitalWrite(A1, HIGH);
+    Serial.println("High2");
   } else {
     digitalWrite(A1, LOW);  
  }  
-  if (cap3 > 1500) {
-    if (pcap3 - cap3 > 500) {
-      digitalWrite(A2, LOW);
-    } else {
-      Serial.println("AHHHHHHHH");
-      digitalWrite(A2, HIGH);
-    }
-  } else {
-    digitalWrite(A2, LOW);  
+ if (pcap3 - cap3 > 500) {
+  digitalWrite(A2, HIGH);
+  Serial.println("High3");
+ } else {
+  digitalWrite(A2, LOW);  
  }
  pcap1 = cap1;
  pcap2 = cap2;
  pcap3 = cap3;
    
 }
-long smooth1() { /* function smooth */
- return cs1.capacitiveSensor(sensitivity);
-}
-long smooth2() { /* function smooth */
-  return cs2.capacitiveSensor(sensitivity);
-}
-long smooth3() { /* function smooth */
-  return cs3.capacitiveSensor(sensitivity);
-}
+
