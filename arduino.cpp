eafd2e63819301abac6d6f1118020c587decad9f
Douglas Wolff <75222401+DouglasAWolff@users.noreply.github.com>
2023-06-12 19:04:16 +0100

    redesigned system

--- arduino.cpp
+++ arduino.cpp
@@ -12,10 +12,6 @@ CapacitiveSensor cs2   = CapacitiveSensor(8, 9);
 CapacitiveSensor cs3   = CapacitiveSensor(12, 11);
 
 void setup() {
-  pinMode(A0, OUTPUT);
-  pinMode(A1, OUTPUT);
-  pinMode(A2, OUTPUT);
-  //Init Serial USB
   Serial.begin(115200);
   Serial.println(F("Initialize System"));
 }
@@ -24,25 +20,16 @@ void loop() {
   int cap1 = cs1.capacitiveSensor(sensitivity);
   int cap2 = cs2.capacitiveSensor(sensitivity);
   int cap3 = cs3.capacitiveSensor(sensitivity);
-  Serial.println(cap1);
-  if (pcap1 < 500 && cap1 < 500) {
-    digitalWrite(A0, LOW);
-  } else {
-    digitalWrite(A0, HIGH);  
-  }
-  if (pcap2 < 500 && cap2 < 500) {
-    digitalWrite(A1, LOW);
-  } else {
-    digitalWrite(A1, HIGH);  
-  }  
- if (pcap3 < 500 && cap3 < 500) {
-  digitalWrite(A2, LOW);
- } else {
-  digitalWrite(A2, HIGH);  
- }
- pcap1 = cap1;
- pcap2 = cap2;
- pcap3 = cap3;
-   
-}
 
+  if (cap1 > 60) {
+    Serial.println(1);
+  }
+  if (cap2 > 60) {
+    Serial.prinln(2);
+  }
+  if (cap3 > 60) {
+    Serial.prinln(3);
+  }
+  
+  Serial.prinln(0)
+}
