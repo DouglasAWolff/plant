7c27653eda90539d7cf7011d881bc3b59c07c142
Douglas Wolff <75222401+DouglasAWolff@users.noreply.github.com>
2023-06-13 17:24:04 +0100

    Update arduino.cpp

--- arduino.cpp
+++ arduino.cpp
@@ -8,26 +8,37 @@ int pcap2 = 0;
 int pcap3 = 0;
 
 CapacitiveSensor cs1   = CapacitiveSensor(2, 3);
-CapacitiveSensor cs2   = CapacitiveSensor(8, 9);
-CapacitiveSensor cs3   = CapacitiveSensor(12, 11);
+CapacitiveSensor cs2   = CapacitiveSensor(2, 9);
+CapacitiveSensor cs3   = CapacitiveSensor(2, 11);
 
 void setup() {
   Serial.begin(115200);
-  
+  cs1.reset_CS_AutoCal();
 }
 
 void loop() {
   int cap1 = cs1.capacitiveSensor(sensitivity);
   int cap2 = cs2.capacitiveSensor(sensitivity);
   int cap3 = cs3.capacitiveSensor(sensitivity);
-  if (cap1 > 100) {
+  // Serial.print(cap1);
+  // Serial.print(" ");
+  // Serial.print(cap2);
+  // Serial.print(" ");
+  // Serial.println(cap3);
+
+
+  if (abs(pcap1 - cap1)> ((cap1 + 2)*5)) {
     Serial.println(1);
   }
-  if (cap2 > 100) {
+  if (abs(pcap2 - cap2)> ((cap2 + 2)*5)) {
     Serial.println(2);
   }
-  if (cap3 > 100) {
-    //Serial.println(cap3);
+  if (abs(pcap3 - cap3)> ((cap3 + 2)*5)) {
     Serial.println(3);
   }
+
+  pcap1 = cap1;
+  pcap2 = cap2;
+  pcap3 = cap3;
+  
 }
