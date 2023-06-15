beab47a47b249b30b3011b41a0fc4a34e47add48
Douglas Wolff <75222401+DouglasAWolff@users.noreply.github.com>
2023-06-15 17:00:56 +0100

    Update arduino.cpp

--- arduino.cpp
+++ arduino.cpp
@@ -1,63 +1,143 @@
-//Libraries
-#include <CapacitiveSensor.h>//https://github.com/PaulStoffregen/CapacitiveSensor
-//Parameters
-const int sensitivity   = 10;
+import serial
+import random
+import time
+from pyo import * 
+import traceback
 
-int pcap1 = 0;
-int pcap2 = 0;
-int pcap3 = 0;
-int acap1 = 0;
-int acap2 = 0;
-int acap3 = 0;
+################################################################################
+#                               pyo setup                                      #
+################################################################################
 
-CapacitiveSensor cs1   = CapacitiveSensor(2, 3);
-CapacitiveSensor cs2   = CapacitiveSensor(2, 9);
-CapacitiveSensor cs3   = CapacitiveSensor(2, 11);
+s = Server()
+s.setInOutDevice(pa_get_output_devices()[0].index('default'))
+s.boot()
+f = Adsr(attack=0.1, decay=1, sustain=0, release=0, dur=1.5, mul=.5)
+a = Sine(freq=340, mul=f).out()
+s.start()
 
-void setup() {
-  Serial.begin(115200);
-  cs1.reset_CS_AutoCal();
-}
-
-void loop() {
-  int cap1 = cs1.capacitiveSensor(sensitivity);
-  int cap2 = cs2.capacitiveSensor(sensitivity);
-  int cap3 = cs3.capacitiveSensor(sensitivity);
-  // Serial.print(cap1);
-  // Serial.print(" ");
-  // Serial.print(cap2);
-  // Serial.print(" ");
-  // Serial.println(cap3);
+f.play()
+time.sleep(0.5)
+f.play()
+time.sleep(0.5)
+################################################################################
+#                            variables setup                                   #
+################################################################################
 
 
-  // if (abs(pcap1 - cap1)> ((cap1 + 2)*3)) {
-  //   Serial.println(1);
-  // }
-  // if (abs(pcap2 - cap2)> ((cap2 + 2)*3)) {
-  //   Serial.println(2);
-  // }
-  // if (abs(pcap3 - cap3)> ((cap3 + 2)*3)) {
-  //   Serial.println(3);
-  // }
 
-  if (cap1 - acap1 >  50) {
-    Serial.println(1);
-  }
-  if (cap2 - acap2 >  50) {
-    Serial.println(2);
-  }
-  if (cap3 - acap3 >  50) {
-    Serial.println(3);
-  }
+################################################################################
+#                        classes/functions setup                               #
+################################################################################
 
-  acap1 = acap1 + 1/2 *(cap1 - acap1);
-  acap2 = acap2 + 1/2 *(cap2 - acap2);
-  acap3 = acap3 + 1/2 *(cap3 - acap3);
+class Plant():
+    def __init__(self):
+        self.thresh = 40
+       
 
-  Serial.println(acap1);
+class Plant1(Plant):
+    def __init__(self):
+        self.duration = 0.3
+        self.semi_intervals = [2.0**(1/12),1/(2.0**(1/12))]
+        self.adsr = Adsr(attack=0.1, decay=0.3, sustain=1, release=.9, dur=0.9, mul=.5)
+        self.synth = SuperSaw(freq=340, mul=self.adsr).out()
+        self.last_trig = time.perf_counter()
 
-  pcap1 = cap1;
-  pcap2 = cap2;
-  pcap3 = cap3;
-  
-}
+    def play(self):
+        if time.perf_counter() - self.last_trig > self.duration:
+            self.synth.freq = self.synth.freq * random.choice(self.semi_intervals)
+            self.adsr.play()
+            self.last_trig = time.perf_counter()
+
+def is_touch(sens):
+    if inputs[-1][sens] > thres[sens]:
+        return True
+    else:
+        return False
+
+
+
+class Plant2(Plant):
+    def __init__(self):
+        self.duration = 1
+        self.semi_intervals = [2.0**(1/12),1/(2.0**(1/12))]
+        self.adsr = Adsr(attack=0.1, decay=0.3, sustain=1, release=.9, dur=self.duration, mul=0.7)
+        self.mod = Sine(freq=6, mul=50)
+        self.synth = Sine(freq= self.mod + 320, mul=self.adsr).out()
+        self.last_trig = time.perf_counter()
+
+    def play(self):
+        if time.perf_counter() - self.last_trig > self.duration:
+            self.synth.freq = self.synth.freq * random.choice(self.semi_intervals)
+            self.adsr.play()
+            self.last_trig = time.perf_counter()
+
+    
+
+
+plant1 = Plant1()
+plant2 = Plant2()
+
+################################################################################
+#                                main loop                                     #
+################################################################################
+zcount = [time.perf_counter() for i in range(6)]
+
+ser = serial.Serial('/dev/ttyS0', '115200', timeout=0.5)
+ser1 = serial.Serial('/dev/ttyACM0', '115200', timeout=0.5)
+try:
+    while True:
+        inp = 0
+        try:
+            ppreinp = ser.readline().decode('utf-8')
+            preinp = ppreinp.replace("\0", "").replace("\n", "").replace("\r", "")
+            if len(preinp) > 0:
+                inp = int(preinp)
+                
+            ser.reset_input_buffer()
+        except Exception:
+            print(traceback.format_exc())
+            time.sleep(0.05)
+        if not inp == 0:
+            if inp == 1 and zcount[0] - time.perf_counter() < -0.3:
+                a.freq = a.freq * random.choice([2.0**(1/12),1/(2.0**(1/12))])
+                f.play()
+                zcount[0] = 0
+
+
+            elif inp == 2 and zcount[1] - time.perf_counter() < -0.3:
+                plant1.play()
+                zcount[1] = 0
+
+            elif inp == 3 and zcount[2] - time.perf_counter() < -0.3:
+                plant2.play()
+                zcount[2] = 0
+
+        inp1 = 0
+        try:
+            ppreinp = ser1.readline().decode("utf-8")
+            preinp = ppreinp.replace("\0", "").replace("\n", "").replace("\r", "")
+
+            if len(preinp) > 0:
+                inp1 = int(preinp)
+            ser1.reset_input_buffer()
+        except Exception:
+            print(traceback.format_exc())
+            time.sleep(0.05)
+        if not inp1 == 0:
+            if inp1 == 4 and zcount[3] - time.perf_counter() < -0.3:
+                a.freq = a.freq * random.choice([2.0**(1/12),1/(2.0**(1/12))])
+                f.play()
+                zcount[3] = 0
+
+
+            elif inp1 == 5 and zcount[4] - time.perf_counter() < -0.3:
+                plant1.play()
+                zcount[4] = 0
+
+            elif inp1 == 6 and zcount[5] - time.perf_counter() < -0.3:
+                plant2.play()
+                zcount[5] = 0
+
+finally:
+    ser.close()
+    ser1.close()
